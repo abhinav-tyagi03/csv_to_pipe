2cc027fe94a2831e454771eecc42af1cbefcc2c9
abhinav-tyagi03 <93082065+abhinav-tyagi03@users.noreply.github.com>
2023-04-22 20:59:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, File, UploadFile
+import csv
+
+app = FastAPI()
+
+@app.post("/csv-to-pipe")
+async def csv_to_pipe(file: UploadFile = File(...)):
+    pipe_file_path = f"{(file.filename)[:-3]}_pipe_converted.txt"
+    with open(pipe_file_path, 'w') as pipe_file:
+        csv_reader = csv.reader((await file.read()).decode('utf-8').splitlines())
+        for row in csv_reader:
+            pipe_file.write('|'.join(row) + '\n')
+    return {"status": "success"}
