722523deb2bcb3cc0f808818652b298922a044b6
abhinav-tyagi03 <93082065+abhinav-tyagi03@users.noreply.github.com>
2023-04-24 01:44:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI, File, UploadFile
 import csv
-
+import os
 app = FastAPI()
 
 @app.post("/csv-to-pipe")
 async def csv_to_pipe(file: UploadFile = File(...)):
-    pipe_file_path = f"{(file.filename)[:-3]}_pipe_converted.txt"
+    pipe_file_path = f"{(file.filename)[:-4]}_pipe_converted.txt"
     with open(pipe_file_path, 'w') as pipe_file:
         csv_reader = csv.reader((await file.read()).decode('utf-8-sig').splitlines())
         for row in csv_reader:
             pipe_file.write('|'.join(row) + '\n')
-    return {"status": "file conversion successfull"}
+        x = (f"{os.getcwd()}\{pipe_file_path}").replace("\\", "/")
+    return {"status": "file converted to pipe SUCCESSFULLY",
+            "file_location": x
+            }
+
+
