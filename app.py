a953f09b42fe8e45d649a115cd19af39d5a42f50
abhinav-tyagi03 <93082065+abhinav-tyagi03@users.noreply.github.com>
2023-04-24 01:20:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 async def csv_to_pipe(file: UploadFile = File(...)):
     pipe_file_path = f"{(file.filename)[:-3]}_pipe_converted.txt"
     with open(pipe_file_path, 'w') as pipe_file:
-        csv_reader = csv.reader((await file.read()).decode('utf-8').splitlines())
+        csv_reader = csv.reader((await file.read()).decode('utf-8-sig').splitlines())
         for row in csv_reader:
             pipe_file.write('|'.join(row) + '\n')
     return {"status": "success"}
